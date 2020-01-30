87e20a115757b2e1da5f153fe694a43fdbc7e9ca
Dr B P Sharma <drbpsharma@outlook.com>
2020-01-30 15:58:47 +0530

    Create methodhiding1.cpp

---
+++ methodhiding1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+class A
+{
+	public:
+		void aboutus()
+		{
+			printf("This is A\n");
+		}
+};
+class B : public A
+{
+	public:
+		void aboutus()
+		{
+			printf("This is B\n");
+		}
+};
+class C : public B
+{
+	public:
+		void aboutus()
+		{
+			printf("This is C\n");
+		}
+};
+
+int main() {
+	A *p;
+	p=new A();
+	p->aboutus();
+	p=new B();
+	p->aboutus();
+	p=new C();
+	p->aboutus();
+	return 0;
+}
